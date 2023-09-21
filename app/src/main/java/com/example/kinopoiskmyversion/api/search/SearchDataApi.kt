be6cd21c348ef0cb38f342319dcd9498ad335d75
mariasokolova0195@gmail.com <mariasokolova0195@gmail.com>
2023-09-21 20:09:52 +0300

    search screen start

--- app/src/main/java/com/example/kinopoiskmyversion/api/search/SearchDataApi.kt
+++ app/src/main/java/com/example/kinopoiskmyversion/api/search/SearchDataApi.kt
@@ -3,6 +3,7 @@ package com.example.kinopoiskmyversion.api.search
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface SearchDataApi {
 
@@ -12,8 +13,9 @@ interface SearchDataApi {
         "X-API-KEY: J0M94EQ-MAGMZ2N-JEYB7A1-0J2QJ10"//куда писать токен?
     )
     @GET(
-        "?page=1&limit=10&query=" //гет запрос для поиска текста, после query должен быть переделанный текст
-    )
+        "?page=1&limit=10&query="
+    )//гет запрос для поиска текста, после query должен быть переделанный текст
+    suspend fun getSearchData(@Query("q")): List<SearchData> //тут где-то должен подключаться recycleView
+    //поч саспенд?
 
-    fun getSearchData(): Call<List<SearchData>> //тут где-то должен подключаться recycleView
 }
